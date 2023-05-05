793792de107d7e096d0612626725441625b80f42
Jonathanzhao02 <zhaojonathan99@gmail.com>
2023-05-05 19:20:19 +0000

    Fixed test

--- fuzz/fuzz_targets/enc_dec.rs
+++ fuzz/fuzz_targets/enc_dec.rs
@@ -9,5 +9,5 @@ use_litcrypt!();
 fuzz_target!(|value: (&[u8], &[u8])| {
     let enc = litcrypt_internal::xor(value.0, value.1);
     let dec = litcrypt_internal::xor(&enc, value.1);
-    assert_eq!(dec, value.1);
+    assert_eq!(dec, value.0);
 });
